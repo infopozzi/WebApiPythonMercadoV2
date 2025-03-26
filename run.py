2f421ff0c19f6860879f223752477118ce929044
FrancaCodex <viniaze2005@gmail.com>
2025-03-26 09:16:11 -0300

    implementação twilio whats

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from src.config.data_base import init_db
 from src.routes import init_routes
+from flask_cors import CORS
+
 
 def create_app():
     """
     Função que cria e configura a aplicação Flask.
     """
     app = Flask(__name__)
+
+    CORS(app)
 
     init_db(app)
 
---
+++ src/Infrastructure/http/whats_app.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+def Enviar_mensagem(telefoneDestino, texto):
+    account_sid = ""
+    auth_token = ""
+    client = Client(account_sid, auth_token)
+
+    from_whatsapp_number = 'whatsapp:+14155238886'
+    to_whatsapp_number = 'whatsapp:+55' + telefoneDestino
+
+    client.messages.create(body=texto,
+                       from_= from_whatsapp_number,
+                       to= to_whatsapp_number)
+
+def Responder_mensagem():
+    pass
